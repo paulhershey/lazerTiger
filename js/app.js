26139227faec82ab2ce9c93cd254c9f2c620b425
Dustin Delatore <dustin@delatore.me>
2018-06-19 14:20:48 +0100

    Add button hover states resolves #11

--- js/app.js
+++ js/app.js
@@ -89,6 +89,19 @@
         })
       });
     }
-  });
 
+    // Button Hovers
+    $('.btn').on('mouseenter', function(e) {
+  		var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+  		$(this).find('span').css({top:relY, left:relX})
+    }).on('mouseout', function(e) {
+  		var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+      $(this).find('span').css({top:relY, left:relX})
+    });
+    $('[href=#]').click(function(){return false});
+  });
 })();
